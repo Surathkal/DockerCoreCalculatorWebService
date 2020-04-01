db3b49ed8818c93bd9494e26738b9f04ccdc78d4
Surathkal <36732414+Surathkal@users.noreply.github.com>
2020-04-01 12:09:19 -0400

    Add project files.

---
+++ DockerCoreCalculatorWebService/DockerCalculateService.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Xml.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace DockerCoreCalculatorWebService
+{
+    public class DockerCalculateService : IDockerCalculateService
+    {
+        private ICalculatorChannel proxy;
+
+
+        public async Task<int> AddAsync(int intA, int intB)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding
+            {
+                SendTimeout = TimeSpan.FromSeconds(10000),
+                MaxBufferSize = int.MaxValue,
+                MaxReceivedMessageSize = int.MaxValue,
+                AllowCookies = true,
+                ReaderQuotas = XmlDictionaryReaderQuotas.Max
+            };
+
+            binding.Security.Mode = BasicHttpSecurityMode.None;
+
+            EndpointAddress address = new EndpointAddress("http://www.dneonline.com/calculator.asmx");
+
+            ChannelFactory<ICalculatorChannel> factory = new ChannelFactory<ICalculatorChannel>(binding, address);
+
+            this.proxy = factory.CreateChannel();
+
+            var result = await this.proxy.AddAsync(intA, intB);
+
+            return result;
+
+        }
+
+
+        public async Task<int> SubtractAsync(int intA, int intB)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding
+            {
+                SendTimeout = TimeSpan.FromSeconds(10000),
+                MaxBufferSize = int.MaxValue,
+                MaxReceivedMessageSize = int.MaxValue,
+                AllowCookies = true,
+                ReaderQuotas = XmlDictionaryReaderQuotas.Max
+            };
+
+            binding.Security.Mode = BasicHttpSecurityMode.None;
+
+            EndpointAddress address = new EndpointAddress("http://www.dneonline.com/calculator.asmx");
+
+            ChannelFactory<ICalculatorChannel> factory = new ChannelFactory<ICalculatorChannel>(binding, address);
+
+            this.proxy = factory.CreateChannel();
+
+            var result = await this.proxy.SubtractAsync(intA, intB);
+
+            return result;
+        }
+
+
+        public async Task<int> MultiplyAsync(int intA, int intB)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding
+            {
+                SendTimeout = TimeSpan.FromSeconds(10000),
+                MaxBufferSize = int.MaxValue,
+                MaxReceivedMessageSize = int.MaxValue,
+                AllowCookies = true,
+                ReaderQuotas = XmlDictionaryReaderQuotas.Max
+            };
+
+            binding.Security.Mode = BasicHttpSecurityMode.None;
+
+            EndpointAddress address = new EndpointAddress("http://www.dneonline.com/calculator.asmx");
+
+            ChannelFactory<ICalculatorChannel> factory = new ChannelFactory<ICalculatorChannel>(binding, address);
+
+            this.proxy = factory.CreateChannel();
+
+            var result = await this.proxy.MultiplyAsync(intA, intB);
+
+            return result;
+        }
+
+
+        public async Task<int> DivideAsync(int intA, int intB)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding
+            {
+                SendTimeout = TimeSpan.FromSeconds(10000),
+                MaxBufferSize = int.MaxValue,
+                MaxReceivedMessageSize = int.MaxValue,
+                AllowCookies = true,
+                ReaderQuotas = XmlDictionaryReaderQuotas.Max
+            };
+
+            binding.Security.Mode = BasicHttpSecurityMode.None;
+
+            EndpointAddress address = new EndpointAddress("http://www.dneonline.com/calculator.asmx");
+
+            ChannelFactory<ICalculatorChannel> factory = new ChannelFactory<ICalculatorChannel>(binding, address);
+
+            this.proxy = factory.CreateChannel();
+
+            var result = await this.proxy.DivideAsync(intA, intB);
+
+            return result;
+        }
+
+
+        public string HelloGreeting(string s)
+        {
+            Console.WriteLine("Test Method Executed!");
+            return s + " Welcome Aboard";
+        }
+
+    }
+}
---
+++ DockerCoreCalculatorWebService/IDockerCalculateService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+
+namespace DockerCoreCalculatorWebService
+{
+    [ServiceContract]
+    public interface IDockerCalculateService
+    {
+
+        [OperationContract]
+        string HelloGreeting(string s);
+
+        [OperationContract]
+        Task<int> AddAsync(int intA, int intB);
+
+        //System.Threading.Tasks.Task<int> AddAsync(int intA, int intB);
+
+        [OperationContract]
+        Task<int> SubtractAsync(int intA, int intB);
+
+        [OperationContract]
+        Task<int> MultiplyAsync(int intA, int intB);
+
+        [OperationContract]
+        Task<int> DivideAsync(int intA, int intB);
+
+    }
+}
+
+
+
